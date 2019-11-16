2885c05149fd818f4677395c6c733c4e2b0f1194
donghuazhang <donghua.zhang98@gmail.com>
2019-11-16 01:29:04 -0500

    feat: 修改

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+import Login from './containers/login'
 
 class App extends Component {
   render() {
-    return <div>I'M READY TO USE THE BACK END APIS! :-)</div>;
+    return (
+      <Login />  
+    );
   }
 }
 
--- client/src/containers/login/index.jsx
+++ client/src/containers/login/index.jsx
@@ -2,11 +2,28 @@
 user login router component
 */
 import React from 'react'
+import SignInForm from './signInForm'
+import SignUpForm from './signUpForm'
 
 export default class Login extends React.Component { 
     render() {
-        return ( 
-            <div>Login</div>
+        return (
+            <div className="container">
+                <article className="main-container">
+                    <div className="tabs">
+                        <span className="tab signin active">
+                            <a href="#signin">Sign In</a>
+                        </span>
+                        <span className="tab signup">
+                            <a href="#signin">Sign Up</a>
+                        </span>
+                    </div>
+                    <div className="content">
+                        <SignInForm />
+                        <SignUpForm />
+                    </div>      
+                </article>
+            </div>
         ) 
     }
 }
