5034879857e08ba7c4f283632c914f88420ab741
donghuazhang <donghua.zhang98@gmail.com>
2019-11-17 18:20:46 -0500

    fix: signup route fixed

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,21 +5,32 @@ const md5 = require('blueimp-md5')
 const UserModel = require('../db/models').UserModel
 //const filter = { password: 0 }
 
+var _ = require('lodash')
+
 router.post('/signup', function(req, res) {
   const { username, password, email } = req.body
   
-  UserModel.findOne({ username }, function(err, user) {
-    if (user) {
-      res.send({ code: 1, msg: 'Username already existed'});
-    } else {
-      new UserModel({username, password: md5(password), email}).save(function (err, user) {
-        //the browser saves the cookie
-        res.cookie('userid', user._id, { maxAge: 1000*60*60*24*7})
-        
-        res.send({ code: 0, data: { _id: user._id, username, email }})
-      })
-    }
+  UserModel.find({ 
+    $or: [
+      { username },
+      { email }
+    ]}, 
+    function(err, users) {
+      if (users.length != 0) {
+        if (_.find(users, {username})) {
+          res.send({ code: 1, msg: 'This username already existed'});
+        } else if (_.find(users, {email})) {
+          res.send({ code: 1, msg: 'This email already existed'});
+        }
+      } else {
+        new UserModel({username, password: md5(password), email}).save(function (err, user) {
+          //the browser saves the cookie
+          res.cookie('userid', user._id, { maxAge: 1000*60*60*24*7})    
+          res.send({ code: 0, data: { _id: user._id, username, email }})
+        })
+      }
   })
+
 })
 
 router.post('/login', function(req, res) {
