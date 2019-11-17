2340bbd5a3f0e907ac465a82b3f554b07e474486
donghuazhang <donghua.zhang98@gmail.com>
2019-11-17 14:33:01 -0500

    feat: delete register folder

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/register', function(req, res, next) {
+  const { username, password } = req.body
+  console.log('register', username, password)
+  if (username === 'admin') {
+    res.send({ code: 1, msg: 'user exists'})
+  } else {
+    res.send({ code: 0, data: {_id: 'abc', username, password}})
+  }
+});
+
 module.exports = router;
