6539487728f698480a0356e595cb4548dda0b675
donghuazhang <donghua.zhang98@gmail.com>
2019-11-17 16:38:15 -0500

    feat: database and service routes created

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,39 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const md5 = require('blueimp-md5')
+const UserModel = require('../db/models').UserModel
+//const filter = { password: 0 }
 
-router.post('/register', function(req, res, next) {
+router.post('/signup', function(req, res) {
+  const { username, password, email } = req.body
+  
+  UserModel.findOne({ username }, function(err, user) {
+    if (user) {
+      res.send({ code: 1, msg: 'Username already existed'});
+    } else {
+      new UserModel({username, password: md5(password), email}).save(function (err, user) {
+        //the browser saves the cookie
+        res.cookie('userid', user._id, { maxAge: 1000*60*60*24*7})
+        
+        res.send({ code: 0, data: { _id: user._id, username, email }})
+      })
+    }
+  })
+})
+
+router.post('/login', function(req, res) {
   const { username, password } = req.body
-  console.log('register', username, password)
-  if (username === 'admin') {
-    res.send({ code: 1, msg: 'user exists'})
-  } else {
-    res.send({ code: 0, data: {_id: 'abc', username, password}})
-  }
-});
 
-module.exports = router;
+  UserModel.findOne({ username, password: md5(password) }, function(err, user) {
+    if (!user) {
+      res.send({ code: 1, msg: 'wrong username or password' })
+    } else {
+        res.cookie('userid', user._id, { maxAge: 1000*60*60*24*7})
+
+        res.send({ code: 0, data: user })
+      }
+  })
+})
+
+module.exports = router
