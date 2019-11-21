1e53efb272238c3f40d1fd1b710f36da70ec6449
weichenluo <charlieluo130@gmail.com>
2019-11-21 13:29:20 -0500

    add: background image for login tab

--- client/src/containers/login/index.jsx
+++ client/src/containers/login/index.jsx
@@ -4,6 +4,9 @@ user login router component
 import React from 'react'
 import './index.scss'
 import img from './img/john-wallin-liberto-thingamajiggre2small.jpg'
+import backgroundimg from './img/close-up-photo-of-painting-3219899.jpg'
+import BackGround from './background'
+
 
 export default class Login extends React.Component { 
     constructor(props) {
@@ -30,71 +33,74 @@ export default class Login extends React.Component {
         }
     }
 
+
     render() {
         return (
-            <div className="container">
-                <div className="first-half">
-                    <h1 className="title">ArtFetch</h1>
-                    <div className="tabs">
-                        <span 
-                            className={`tab signin ${this.state.activeTab === 0 ? 'active' : null}`}
-                            onClick = {() => this.switchTabs('signin')}
-                        >
-                            <a href="#signin">Log In</a>
-                        </span>
-                        <span 
-                            className={`tab signup ${this.state.activeTab === 1 ? 'active' : null}`}
-                            onClick = {() => this.switchTabs('signup')}
-                        >
-                            <a href="#signin">Sign Up</a>
-                        </span>
+            
+                <div className="container">
+                    <div className="first-half">
+                        <h1 className="title">ArtFetch</h1>
+                        <div className="tabs">
+                            <span 
+                                className={`tab signin ${this.state.activeTab === 0 ? 'active' : null}`}
+                                onClick = {() => this.switchTabs('signin')}
+                            >
+                                <a href="#signin">Log In</a>
+                            </span>
+                            <span 
+                                className={`tab signup ${this.state.activeTab === 1 ? 'active' : null}`}
+                                onClick = {() => this.switchTabs('signup')}
+                            >
+                                <a href="#signin">Sign Up</a>
+                            </span>
+                        </div>
+                        <div className="content">
+                            <div 
+                                className="signin-content" 
+                                style={{display: this.state.activeTab === 0 ? 'block' : 'none'}}
+                            >
+                                <form action="#" method="post">
+                                    <div className="input-group">
+                                        <input className="input" placeholder="Username" />
+                                        <input className="input" placeholder="Password" />
+                                    </div>
+                                    <div className="submit-group">
+                                        <input type="submit" className="submit" value="Log in" />
+                                        <div className="extra">
+                                            <a href="#" className="extra">Forgot your password?</a>
+                                        </div>   
+                                    </div>
+                                </form>
+                            </div>
+                            <div 
+                                className="signup-content" 
+                                style={{display: this.state.activeTab===1 ? 'block' : 'none'}}
+                            >
+                                <form action="#" method="post">
+                                    <div className="input-group">
+                                        <input className="input" placeholder="Username" />
+                                        <input className="input" placeholder="Email" />
+                                        <input className="input" placeholder="Password" />
+                                        <input className="input" placeholder="Password confirm" />
+                                    </div>
+                                    <div className="submit-group">
+                                        <input type="submit" className="submit" value="Sign up" />
+                                    </div>
+                                </form>
+                            </div>
+                        </div>      
                     </div>
-                    <div className="content">
+                    <div className="second-half">
                         <div 
-                            className="signin-content" 
-                            style={{display: this.state.activeTab === 0 ? 'block' : 'none'}}
-                        >
-                            <form action="#" method="post">
-                                <div className="input-group">
-                                    <input className="input" placeholder="Username" />
-                                    <input className="input" placeholder="Password" />
-                                </div>
-                                <div className="submit-group">
-                                    <input type="submit" className="submit" value="Log in" />
-                                    <div className="extra">
-                                        <a href="#" className="extra">Forgot your password?</a>
-                                    </div>   
-                                </div>
-                            </form>
-                        </div>
-                        <div 
-                            className="signup-content" 
-                            style={{display: this.state.activeTab===1 ? 'block' : 'none'}}
-                        >
-                            <form action="#" method="post">
-                                <div className="input-group">
-                                    <input className="input" placeholder="Username" />
-                                    <input className="input" placeholder="Email" />
-                                    <input className="input" placeholder="Password" />
-                                    <input className="input" placeholder="Password confirm" />
-                                </div>
-                                <div className="submit-group">
-                                    <input type="submit" className="submit" value="Sign up" />
-                                </div>
-                            </form>
-                        </div>
-                    </div>      
+                            className="image" 
+                            style={{
+                                background: `url(${img})`,
+                                backgroundSize: '100%',
+                            }} 
+                        />
+                    </div>
                 </div>
-                <div className="second-half">
-                    <div 
-                        className="image" 
-                        style={{
-                            background: `url(${img})`,
-                            backgroundSize: '100%',
-                        }} 
-                    />
-                </div>
-            </div>
+           
         ) 
     }
 }
