9085446bfd41928c2ff04060f4f297b5b9f61eea
donghuazhang98 <46079140+donghuazhang98@users.noreply.github.com>
2019-11-23 01:34:37 -0500

    Update index.jsx

--- client/src/containers/login/background/index.jsx
+++ client/src/containers/login/background/index.jsx
@@ -1,19 +1,1 @@
-import React, {Component} from 'react';
-import img from './img/close-up-photo-of-painting-3219899.jpg'
 
-class BackgroundImg extends Component{
-    render(){
-        const style = {
-            background: `url(${img})`,
-            backgroundSize: '100%，100%',
-            width: '100%',
-            height: '100%',
-        }
-
-        return(
-            <div style = {{...style}}></div>
-        )
-    }
-}
-
-export default BackgroundImg;
