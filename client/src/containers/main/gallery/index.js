aba4c8edcb7b6e007a7ddfcaacb8fcf861704ee9
weichenluo <charlieluo130@gmail.com>
2019-12-07 21:09:53 -0500

    add: carousel & gallery

---
+++ client/src/containers/main/gallery/index.js
@@ -0,0 +1,49 @@
+import React, { useState, useCallback } from "react";
+import ReactDOM from "react-dom";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "./photos";
+
+import "./styles.css";
+
+function App() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  return (
+    <div>
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
+  );
+}
+
+const BasicRows = () => <Gallery photos={photos} />;
+
+export default App;
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
--- client/src/containers/main/index.jsx
+++ client/src/containers/main/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import DemoCarousel from './carousel'
+import DemoGallery from './gallery'
 
 export default class Main extends React.Component {
     render() {
         return(
-            <div>main</div>
+            <div>main
+                <div>
+
+                    <DemoCarousel/>
+
+                </div>
+                <div>
+                    <DemoGallery/>
+                </div>
+            </div>
+
         )
     }
 }
